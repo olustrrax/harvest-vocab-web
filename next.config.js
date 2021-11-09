6ac019ffa3a7e799657ff5c6bd6bc37e2f43f052
Dusadee <dusadee.s@globish.co.th>
2021-11-09 14:16:48 +0700

    add prettier eslint

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withAntdLess = require('next-plugin-antd-less')
+// module.exports =withAntdLess({
+//   cssLoaderOptions: {},
+
+//   // Other Config Here...
+
+//   webpack(config) {
+//     return config;
+//   },
+
+//   reactStrictMode: true,
+// })
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer(
+  withAntdLess({
+    cssLoaderOptions: {},
+
+    // Other Config Here...
+
+    webpack(config) {
+      return config
+    },
+
+    reactStrictMode: true
+  })
+)
