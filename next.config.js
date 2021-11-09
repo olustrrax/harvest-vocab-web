24d8db68c4256f670a6cf0526742bcef7bad58f9
Dusadee <dusadee.s@globish.co.th>
2021-11-09 14:34:13 +0700

    add style commonent

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withAntdLess = require('next-plugin-antd-less')
-// module.exports =withAntdLess({
-//   cssLoaderOptions: {},
 
-//   // Other Config Here...
-
-//   webpack(config) {
-//     return config;
-//   },
-
-//   reactStrictMode: true,
-// })
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
@@ -18,7 +8,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer(
   withAntdLess({
     cssLoaderOptions: {},
-
     // Other Config Here...
 
     webpack(config) {
